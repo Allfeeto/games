7501a301a03fc83496f848d6e8c9371489a41fbe
Allfeeto <10509090S@mail.ru>
2025-03-04 16:23:33 +0300

    коммент перед изменениями в сервлетах и страницах

---
+++ src/main/java/DAO/ConnectionProperty.java
@@ -0,0 +1,26 @@
+package DAO;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class ConnectionProperty {
+	
+	public static final String CONFIG_NAME = "config.properties";
+	public static final Properties GLOBAL_COFIG = new Properties();
+	
+	public ConnectionProperty()
+		throws FileNotFoundException, IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		GLOBAL_COFIG.load(classLoader.getResourceAsStream("config/"
+		+CONFIG_NAME));
+	}
+	// Получить значение параметра из конфигурации по имени свойства
+	public static String getProperty(String property) {
+		return GLOBAL_COFIG.getProperty(property);
+	}
+	
+
+}
